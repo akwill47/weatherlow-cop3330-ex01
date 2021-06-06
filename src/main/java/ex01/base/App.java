b7ee844a37fda2bf9cdab27b0ede7f6533196525
Akwill47 <wil.weat@hotmail.com>
2021-06-06 19:18:50 -0400

    I

--- src/main/java/ex01/base/App.java
+++ src/main/java/ex01/base/App.java
@@ -1,18 +1,8 @@
 package ex01.base;
 import java.util.Scanner;
 /*
-Exercise 1 - Saying Hello
-Create a program that prompts for your name and prints a greeting using your name.
-
-Example Output
-What is your name? Brian
-Hello, Brian, nice to meet you!
-Constraint
-Keep the input, string concatenation, and output separate.
-Challenges
-Write a new version of the program without using any variables.
-Write a version of the program that displays different greetings for different people.
-Write a version of the program that displays the greeting in a GUI window.
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 William Weatherlow
  */
 public class App {
     static Scanner in = new Scanner(System.in);
@@ -24,7 +14,7 @@ public class App {
 
         String outputString = myApp.generateOutputString(name);
 
-        printOutput(outputString);
+        myApp.printOutput(outputString);
 
     }
 
